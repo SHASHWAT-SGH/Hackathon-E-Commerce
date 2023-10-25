73b42c25b17cd7d48a4da94bc3464ab3e5748297
aryanpnd <captainaryan9870@gmail.com>
2023-10-25 18:31:55 +0530

    changed URI

--- backend/routes/api/allApiRoutes.js
+++ backend/routes/api/allApiRoutes.js
@@ -2,12 +2,13 @@ const express = require('express');
 const { productRoutes } = require('./product/products');
 const { UserRoutes } = require('./user/users');
 const { WishlistRoutes } = require('./user/wishlist');
+const { CartRoutes } = require('./user/cart');
 
 const protectedApiRoutes = express.Router()
 const unprotectedApiRoutes = express.Router()
 
 protectedApiRoutes.use('/',
-    UserRoutes,WishlistRoutes
+    UserRoutes,WishlistRoutes,CartRoutes
 )
 unprotectedApiRoutes.use('/',
     productRoutes
---
+++ backend/routes/api/user/cart.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const { addToCart, getCart, check } = require('../../../controller/users/cart');
+
+const CartRoutes = express.Router()
+
+
+CartRoutes.get('/getCart', check, getCart)
+    .post('/addToCart', check, addToCart)
+    // .delete('/deleteOneFromCart', check, deleteOneFromCart)
+    // .delete('/deleteAllFromCart', check, deleteAllFromCart)
+
+
+
+module.exports = { CartRoutes }
